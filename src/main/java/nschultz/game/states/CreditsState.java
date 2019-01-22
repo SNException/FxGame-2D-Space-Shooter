e6f07cadbb75844ee360a6cd4aa36963ae7e5036
Niklas Schultz <thexsalkin@gmail.com>
2019-01-22 17:46:37 +0100

    Refactored CreditsState
    The hardcoded license string is way more readable now.
    I replaced the manuel string concatination with a String.join
    call.

--- src/main/java/nschultz/game/states/CreditsState.java
+++ src/main/java/nschultz/game/states/CreditsState.java
@@ -34,29 +34,29 @@ import nschultz.game.ui.GameCanvas;
 
 public class CreditsState extends GameState {
 
-    private static final String NEW_LINE = System.lineSeparator();
-    private static final String MIT_LICENSE = "The MIT License" + NEW_LINE +
-            NEW_LINE +
-            "      Copyright 2019 Niklas Schultz." + NEW_LINE +
-            NEW_LINE +
-            "      Permission is hereby granted, free of charge, to any person obtaining a copy" + NEW_LINE +
-            "      of this software and associated documentation files (the \"Software\"), to deal" + NEW_LINE +
-            "      in the Software without restriction, including without limitation the rights" + NEW_LINE +
-            "      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell" + NEW_LINE +
-            "      copies of the Software, and to permit persons to whom the Software is" + NEW_LINE +
-            "      furnished to do so, subject to the following conditions:" + NEW_LINE +
-            NEW_LINE +
-            "      The above copyright notice and this permission notice shall be included in" + NEW_LINE +
-            "      all copies or substantial portions of the Software." + NEW_LINE +
-            NEW_LINE +
-            "      THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR" + NEW_LINE +
-            "      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY," + NEW_LINE +
-            "      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE" + NEW_LINE +
-            "      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER" + NEW_LINE +
-            "      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM," + NEW_LINE +
-            "      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN" + NEW_LINE +
-            "      THE SOFTWARE.";
-
+    private static final String MIT_LICENSE = String.join(
+            System.lineSeparator(),
+            "The MIT License",
+            "",
+            "Copyright 2019 Niklas Schultz.",
+            "Permission is hereby granted, free of charge, to any person obtaining a copy",
+            "of this software and associated documentation files (the \"Software\"), to deal",
+            "in the Software without restriction, including without limitation the rights",
+            "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell",
+            "copies of the Software, and to permit persons to whom the Software is",
+            "furnished to do so, subject to the following conditions:",
+            "",
+            "The above copyright notice and this permission notice shall be included in",
+            "all copies or substantial portions of the Software.",
+            "",
+            "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR",
+            "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,",
+            "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE",
+            "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER",
+            "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,",
+            "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN",
+            "THE SOFTWARE."
+    );
 
     private double y = game().resolution().getHeight() + 400;
 
@@ -67,9 +67,10 @@ public class CreditsState extends GameState {
     @Override
     public void update(final long now) {
         if (y == -400) {
-            if (this.lastGameState()==null)
+            if (this.lastGameState() == null)
                 game().switchGameState(new MenuState(game()));
-            else game().switchGameState(new MenuState(game(), this.lastGameState()));
+            else
+                game().switchGameState(new MenuState(game(), this.lastGameState()));
         } else {
             y -= 1;
         }
